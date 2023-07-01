465ee626c9b0cec055d9854eae598f45665074b4
Makar90 <Makarlancer@gmail.com>
2023-07-01 17:24:33 +0300

    1

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,57 @@
+import {
+    createBrowserRouter,
+    Outlet,
+    RouterProvider,
+} from "react-router-dom";
 
+import Home from "./pages/Home/Home";
+import Products from "./pages/Product/Product";
+import Product from "./pages/Products/Products";
+
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+
+
+
+const Layout = () => {
+    return (
+        <div className="app">
+            <Header />
+            <Navbar />
+            <Outlet />
+            <Footer />
+        </div>
+    )
+}
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            },
+            {
+                path: "/products/:id",
+                element: <Products />
+            },
+            {
+                path: "/product/:id",
+                element: <Product />
+            },
+        ]
+    },
+]);
 
 function App() {
+
     return (
         <div className="App">
-            123_45
+            <RouterProvider router={router} />
         </div>
     );
 }
